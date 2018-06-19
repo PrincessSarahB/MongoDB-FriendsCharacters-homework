7881e2eea7e0d4fb508996a4847d4b60456ed58b
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-19 15:19:57 +0100

    started setting up db

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const server = express();
 server.use(parser.json());
 server.use(parser.urlencoded({extended: true}));
 
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+  if(err) {
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("friends_characters");
+  console.log("connected to database!");
+
+  
+
 server.listen(3000, function(){
   console.log("Listening on port 3000");
 });
