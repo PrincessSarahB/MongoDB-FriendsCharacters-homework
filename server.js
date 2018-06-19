c835f894f2cf28831dd9b3a4c85045099e76bdf6
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-19 15:11:43 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+
+server.use(parser.json());
+server.use(parser.urlencoded({extended: true}));
+
+server.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
